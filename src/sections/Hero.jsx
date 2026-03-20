c8354cca9a5c572332a2cbc88312fd26e79006bc
Hell086 <binayshrestha086@gmail.com>
2026-03-20 11:42:51 +0545

    portfolio commited

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,8 +4,10 @@ import { useMediaQuery } from "react-responsive";
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
-    <section className="flex items-start justify-center md:items-start md:justify-start c-space min-h-screen"
-    id="home">
+    <section
+      className="flex items-start justify-center md:items-start md:justify-start c-space pt-24 pb-10"
+      id="home"
+    >
       <HeroText />
     </section>
   );
