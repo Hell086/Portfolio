cda45424ca8f57aed79de0cee6b35d380ed4ce79
Hell086 <binayshrestha086@gmail.com>
2026-03-26 08:51:12 +0545

    latest commited

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
     <section
-      className="flex items-center justify-center md:items-center md:justify-start hero-section"
+      className="flex items-center justify-center md:items-center md:justify-start c-space section-spacing hero-section"
       id="home"
     >
       <HeroText />
