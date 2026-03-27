338f5ad3b765909b4946c2af6c42c324704d8297
Hell086 <binayshrestha086@gmail.com>
2026-03-27 09:15:50 +0545

    latest commited

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,6 @@
 import HeroText from "../components/HeroText";
-import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-  const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
     <section
       className="flex items-center justify-center md:items-center md:justify-start c-space section-spacing hero-section"
