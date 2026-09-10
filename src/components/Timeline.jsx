aa2615f8c05cfea63cd4e4062f17de868e0d0c67
Hell086 <binayshrestha086@gmail.com>
2026-09-10 21:51:53 +0545

    commited

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -118,7 +118,7 @@ export const Timeline = ({ data }) => {
             whileTap={{ scale: 0.95 }}
             onClick={() => window.open("/Binay_Shrestha_CV.pdf", "_blank")}
           >
-            View Resume
+            View CV
           </motion.button>
       </div>
     </div>
